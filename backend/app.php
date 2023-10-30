2ac2e7982d7739d39377e7ca4cdf84a462798bc6
s-ri <s-ri@m-p-j.com>
2023-10-30 15:46:13 +0900

    app cors update

--- backend/app.php
+++ backend/app.php
@@ -11,34 +11,34 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
+$allowedOrigins = array(
+    'http://example.com',
+    'https://example.com',
+    'http://localhost:8080',
+    'http://localhost:8300',
+    'http://host.docker.internal:8080',
+);
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : 'http://host.docker.internal:8080';
+if (!in_array($origin, $allowedOrigins)) {
+    header('HTTP/1.0 403 Forbidden');
+    echo "Access denied.";
+    exit;
+}
+
+
 if (preg_match('/\/([a-z]+)\/([a-zA-Z0-9]+)\/([a-zA-Z0-9]+)$/', $uri, $matches)) {
     $value1 = $matches[1];
     $value2 = $matches[2];
     $value3 = $matches[3];
 
     if ($value1 === 'api' && $value2 === 'job' && file_exists('static/job/'.$value3.'.json')){
-
-        $allowedOrigins = array(
-            'http://example.com',
-            'https://example.com',
-            'http://localhost:8080',
-            'http://localhost:8300',
-            'http://host.docker.internal:8080',
-        );
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : 'http://host.docker.internal:8080';
-        if (in_array($origin, $allowedOrigins)) {
-            header('Access-Control-Allow-Origin: ' . $origin);
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-            header('Access-Control-Allow-Headers: Content-Type');
-            header('Access-Control-Allow-Credentials: true');
-            header('Content-Type:text/json;charset=utf-8');
-            echo file_get_contents('static/job/'.$value3.'.json');
-            exit;
-        } else {
-            header('HTTP/1.0 403 Forbidden');
-            echo "Access denied.";
-            exit;
-        }
+        header('Access-Control-Allow-Origin: ' . $origin);
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type');
+        header('Access-Control-Allow-Credentials: true');
+        header('Content-Type:text/json;charset=utf-8');
+        echo file_get_contents('static/job/'.$value3.'.json');
+        exit;
     }
 }
 
