3f06e2d5a0dd7832d74da5b2d0f8db08f8a37d04
s-ri <s-ri@m-p-j.com>
2024-06-12 13:16:05 +0900

    Hello page ssr load

--- frontend/app/app/hello/page.tsx
+++ frontend/app/app/hello/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 // 动态导入客户端组件
-const HelloClient = dynamic(() => import('./HelloServer'), { ssr: false });
+const HelloClient = dynamic(() => import('./HelloServer'), { ssr: true });
 
 const HelloPage = () => {
     return <HelloClient />;
